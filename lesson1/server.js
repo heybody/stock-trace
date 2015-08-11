d044f187a10fa01f6efe91695936115038c9de6f
heybody <ifiamanass@gmail.com>
2015-08-11 15:37:29 +0000

    finish handler

--- lesson1/server.js
+++ lesson1/server.js
@@ -1,16 +1,13 @@
 var http = require("http");
 var url = require("url");
 
-function start(route,handlers){
+function start(route,handle){
   function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for "+ pathname +" received.");
+    //console.log("Request for "+ pathname +" received.");
 
-    route(pathname);
-    var fn  = handlers[pathname];
-    if(typeof fn == "function"){
-        fn();
-    }
+    route(handle,pathname);
+   
     response.writeHead(200,{"Content-Type":"text/plain"});
     response.write("Hello Waaorld");
     response.end();
