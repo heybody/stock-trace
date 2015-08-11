2b9d4cd960284014c8b9a49e37ce80fd23b1b972
heybody <ifiamanass@gmail.com>
2015-08-11 23:53:33 +0800

    Update server.js

--- lesson1/server.js
+++ lesson1/server.js
@@ -1,21 +1,6 @@
 var http = require("http");
 var url = require("url");
 
-<<<<<<< HEAD
-function start(route,handlers){
-  function onRequest(request, response){
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for "+ pathname +" received.");
-
-    route(pathname);
-    handlers[pathname](request);
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(process.env.PORT, process.env.IP);;
-=======
 function start(route,handle){
   function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
@@ -23,7 +8,6 @@ function start(route,handle){
   }
 
   http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
->>>>>>> lesson1
   console.log("Server has started.");
 }
 
