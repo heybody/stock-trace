1851d9ac3b7112f8ebf5a76ac4f3b8f8c3497e1c
heybody <ifiamanass@gmail.com>
2015-08-11 15:41:46 +0000

    a easy web server

--- lesson1/server.js
+++ lesson1/server.js
@@ -4,13 +4,7 @@ var url = require("url");
 function start(route,handle){
   function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
-    //console.log("Request for "+ pathname +" received.");
-
-    route(handle,pathname);
-   
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello Waaorld");
-    response.end();
+    route(handle,pathname,request,response);
   }
 
   http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
