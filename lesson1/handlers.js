3001aa48b44b8ed21bcbab1132a4d24c61aef242
heybody <ifiamanass@gmail.com>
2015-08-13 12:07:13 +0000

    output a form

--- lesson1/handlers.js
+++ lesson1/handlers.js
@@ -2,15 +2,21 @@ var exec = require("child_process").exec;
 
 function start(request, response) {
     var content = '';
-    exec("ls -lah", function(error, stdout, stderr) {
-        content = stdout;
-        console.log("Request handler 'start' was called.");
-        response.writeHead(200, {
-            "Content-Type": "text/plain"
-        });
-        response.write(content);
-        response.end();
-    });
+    var body ='<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
 
 }
 
